e84f501ffd37b180a5438ffdeff7d2348058a349
Aziza0011 <khakimovaaziza95@gmail.com>
2024-07-04 12:08:48 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let ism = prompt('Soz kiriting!')
+
+let body = document.querySelector('body')
+
+let yangikod = document.createElement('h1')
+
+body.appendChild(yangikod)
+yangikod.textContent = ism
+
+
